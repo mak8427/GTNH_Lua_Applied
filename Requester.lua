95a6e69663400eade246399b2782869ec0efae0f
Davide <davide.mattioli.2001@gmail.com>
2025-03-21 17:03:00 +0100

    Debugging

--- Requester.lua
+++ Requester.lua
@@ -7,103 +7,108 @@ os = require("os")
 -- Change to the address of your adapter and redstone io
 -- rs = component.proxy("b029eedf-5119-4aea-91d2-84e3c2e0e4e8")
 
-
--- List of items to watch. Enable advanced tooltips to get the itemnames (F3+H)
--- Add /X to end of item to get specific items based on metadata
--- Each item in this list needs to have a recipe in the system to craft it.
--- Below are most of the mystical aggriculture items from PO3
+-- List of items to watch
 watchitems = {
   ["minecraft:stone"] = 100000,
 }
 
-
 local component = require("component")
 
-
+print("[DEBUG] Checking AE2 component availability...")
 local ae2
 if component.isAvailable("me_controller") then
     ae2 = component.me_controller
+    print("[DEBUG] Connected to ME Controller.")
 elseif component.isAvailable("me_interface") then
     ae2 = component.me_interface
+    print("[DEBUG] Connected to ME Interface.")
 else
-    print("You need to connect the adapter to either a me controller or a me interface")
+    print("[ERROR] No ME controller or interface found. Exiting.")
     os.exit()
 end
 
--- Infinite loop, can use alt-ctrl-c to break
+-- Infinite loop
 while true do
+  print("\n[DEBUG] Starting new loop cycle...")
 
-  -- Check to see if we're getting redstone input from the top.
+  -- Check redstone signal (stubbed)
   rs_input = 100
+  print(string.format("[DEBUG] Redstone input is %d", rs_input))
 
-  -- Check if any CPU's are currently being used.
+  -- Check if any CPUs are busy
+  print("[DEBUG] Fetching AE2 CPU usage...")
   cpus = ae2.getCpus()
-
   busy = false
-  for i in ipairs(cpus) do busy = cpus[i].busy or busy end
+  for i in ipairs(cpus) do
+    print(string.format("[DEBUG] CPU %d busy: %s", i, tostring(cpus[i].busy)))
+    busy = cpus[i].busy or busy
+  end
 
-  -- If no CPU's are being used, and we're not getting a redstone signal from manual kill lever
-  -- then we will run a loop of itemchecking.
   if rs_input > 0 and busy == false then
+    print("[DEBUG] System is idle and ready for crafting check.")
 
-    -- Iterate through each item in watchitems table
-    for itemname,keepsize in pairs(watchitems) do
+    for itemname, keepsize in pairs(watchitems) do
+      print("\n----------------------------------------")
+      print(string.format("[DEBUG] Checking item: %s | Desired count: %d", itemname, keepsize))
 
-      -- String parsing to get out the damage/metadata value from our string
-      -- thermalfoundation:material/2048 becomes
-      --   itemname = thermalfoundation:material
-      --   damage = 2048
-      -- Anything without a metadata specified is 0
-      if string.find(itemname,"/") ~= nil then
+      local damage = 0
+      if string.find(itemname, "/") ~= nil then
         delim = string.find(itemname, "/")
         len = string.len(itemname)
-        damage = string.sub(itemname, delim + 1, len )
-        itemname = string.sub(itemname, 1 , delim - 1 )
+        damage = string.sub(itemname, delim + 1, len)
+        itemname = string.sub(itemname, 1, delim - 1)
+        print(string.format("[DEBUG] Parsed item: %s | Metadata: %s", itemname, damage))
       else
-        damage = 0
+        print("[DEBUG] No metadata found; using 0")
       end
 
-      -- Query AE2 to find items in network
+      print("[DEBUG] Querying AE2 network for item...")
       item_in_network = ae2.getItemsInNetwork({name = itemname, damage = tonumber(damage)})[1]
 
-      -- If response is nil, the item doesn't exist in the network, so we have 0
+      local stocksize = 0
       if item_in_network == nil then
+        print("[DEBUG] Item not found in network. Assuming 0 in stock.")
         stocksize = 0
       else
         stocksize = item_in_network.size
+        print(string.format("[DEBUG] Current stock: %d", stocksize))
       end
 
-      -- If we have less than we want...
       if keepsize > stocksize then
         reqsize = keepsize - stocksize
+        print(string.format("[DEBUG] Need to craft %d more of %s/%s", reqsize, itemname, damage))
 
-        -- This checks the AE2 system to find the crafting recipe with that name/damage
         recipe = ae2.getCraftables({name = itemname, damage = tonumber(damage)})[1]
 
-        -- Recipe not found, probably a typo or you need to set up the recipe
         if recipe == nil then
-          print(string.format("No recipe found for %s / %s, recipe not found", itemname, damage))
+          print(string.format("[ERROR] No recipe found for %s/%s", itemname, damage))
         else
-          print(string.format("%s/%s: Have %s, Need %s ", itemname, damage, stocksize, keepsize))
-
-          -- Request that the AE2 system craft the recipe. Returns a monitor object
-          -- Note that if we don't have enough items in the system to actually craft the amount requested
-          -- we have no way of finding that out, the API just tells us that the craft was done immediately.
+          print(string.format("[DEBUG] Crafting %d of %s/%s...", reqsize, itemname, damage))
           monitor = recipe.request(reqsize)
 
-          -- Wait for the craft to complete
-          while monitor.isDone() == false and monitor.isCanceled() == false do
+          -- Monitor crafting progress
+          while not monitor.isDone() and not monitor.isCanceled() do
+            print("[DEBUG] Crafting in progress...")
             os.sleep(5)
           end
-          print(string.format("%s/%s: crafted %s", itemname,damage,reqsize))
+
+          if monitor.isCanceled() then
+            print(string.format("[WARNING] Crafting of %s/%s was canceled.", itemname, damage))
+          else
+            print(string.format("[DEBUG] Crafting complete: %d of %s/%s", reqsize, itemname, damage))
+          end
         end
+      else
+        print(string.format("[DEBUG] Stock sufficient: %d / %d", stocksize, keepsize))
       end
 
-      -- Wait a bit so as not to hammer to the system
+      -- Delay to reduce network hammering
       os.sleep(5)
     end
+  else
+    print("[DEBUG] Skipping crafting check. Either redstone signal is off or system is busy.")
   end
 
-  -- Wait a bit so as not to hammer to the system
+  -- Main loop delay
   os.sleep(30)
 end
